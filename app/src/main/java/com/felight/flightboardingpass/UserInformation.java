6332fcdcc18dcb89bf5e17cd1ff77b152c6e968f
Sourabh J Kalmady <sourabhjkalmady@gmail.com>
2017-05-11 17:02:12 +0530

    User Information

--- app/src/main/java/com/felight/flightboardingpass/UserInformation.java
+++ app/src/main/java/com/felight/flightboardingpass/UserInformation.java
@@ -1,13 +1,36 @@
 package com.felight.flightboardingpass;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class UserInformation extends AppCompatActivity {
+    private TextView tvName;
+    private TextView tvDOB;
+    private TextView tvNumber;
+    private TextView tvMail;
+    private String DEFAULT = "N/A";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_information);
+        tvName = (TextView) findViewById(R.id.tvName);
+        tvDOB = (TextView) findViewById(R.id.tvDOB);
+        tvNumber = (TextView) findViewById(R.id.tvNumber);
+        tvMail = (TextView) findViewById(R.id.tvMail);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("Registration Data", Context.MODE_PRIVATE);
+        String name = sharedPreferences.getString("Name",DEFAULT);
+        String dob = savedInstanceState.getString("DOB",DEFAULT);
+        String mailId = sharedPreferences.getString("MailId",DEFAULT);
+        String contactNumber = sharedPreferences.getString("ContactNumber",DEFAULT);
+
+        tvName.setText(name);
+        tvDOB.setText(dob);
+        tvNumber.setText(contactNumber);
+        tvMail.setText(mailId);
     }
 }
